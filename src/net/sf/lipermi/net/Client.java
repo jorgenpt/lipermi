fcc76226ed7da8c183137a444159596b63047a12
Jørgen P. Tjernø <jtjerno@mylookout.com>
2011-11-08 14:50:42 -0800

    Whitespace cleanup, modelines.

--- src/net/sf/lipermi/net/Client.java
+++ src/net/sf/lipermi/net/Client.java
@@ -1,21 +1,21 @@
 /*
  * LipeRMI - a light weight Internet approach for remote method invocation
  * Copyright (C) 2006  Felipe Santos Andrade
- * 
+ *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
- * 
+ *
  * For more information, see http://lipermi.sourceforge.net/license.php
  * You can also contact author through lipeandrade@users.sourceforge.net
  */
@@ -41,50 +41,52 @@ import net.sf.lipermi.handler.filter.IProtocolFilter;
  * Connects to a LipeRMI Server in a address:port
  * and create local dynamic proxys to call remote
  * methods through a simple interface.
- * 
+ *
  * @author lipe
  * @date   05/10/2006
- * 
+ *
  * @see    net.sf.lipermi.handler.CallHandler
  * @see    net.sf.lipermi.net.Server
  */
 public class Client {
 
-	private Socket socket;
-	
-	private ConnectionHandler connectionHandler;
-	
-	private final IConnectionHandlerListener connectionHandlerListener = new IConnectionHandlerListener() {
-		public void connectionClosed() {
-			for (IClientListener listener : listeners)
-				listener.disconnected();
-		}
-	};
-	
-	private List<IClientListener> listeners = new LinkedList<IClientListener>();
-	
-	public void addClientListener(IClientListener listener) {
-		listeners.add(listener);
-	}
+    private Socket socket;
 
-	public void removeClientListener(IClientListener listener) {
-		listeners.remove(listener);
-	}	
-	
-	public Client(String address, int port, CallHandler callHandler) throws IOException {
-		this(address, port, callHandler, new DefaultFilter());
-	}
-	
-	public Client(String address, int port, CallHandler callHandler, IProtocolFilter filter) throws IOException {
-		socket = new Socket(address, port);
-		connectionHandler = ConnectionHandler.createConnectionHandler(socket, callHandler, filter, connectionHandlerListener);
-	}
-	
-	public void close() throws IOException {
-		socket.close();
-	}
-	
-	public Object getGlobal(Class<?> clazz) {
-		return Proxy.newProxyInstance(clazz.getClassLoader(), new Class[] { clazz }, new CallProxy(connectionHandler));
-	}
+    private ConnectionHandler connectionHandler;
+
+    private final IConnectionHandlerListener connectionHandlerListener = new IConnectionHandlerListener() {
+        public void connectionClosed() {
+            for (IClientListener listener : listeners)
+                listener.disconnected();
+        }
+    };
+
+    private List<IClientListener> listeners = new LinkedList<IClientListener>();
+
+    public void addClientListener(IClientListener listener) {
+        listeners.add(listener);
+    }
+
+    public void removeClientListener(IClientListener listener) {
+        listeners.remove(listener);
+    }
+
+    public Client(String address, int port, CallHandler callHandler) throws IOException {
+        this(address, port, callHandler, new DefaultFilter());
+    }
+
+    public Client(String address, int port, CallHandler callHandler, IProtocolFilter filter) throws IOException {
+        socket = new Socket(address, port);
+        connectionHandler = ConnectionHandler.createConnectionHandler(socket, callHandler, filter, connectionHandlerListener);
+    }
+
+    public void close() throws IOException {
+        socket.close();
+    }
+
+    public Object getGlobal(Class<?> clazz) {
+        return Proxy.newProxyInstance(clazz.getClassLoader(), new Class[] { clazz }, new CallProxy(connectionHandler));
+    }
 }
+
+// vim: ts=4:sts=4:sw=4:expandtab
--- src/net/sf/lipermi/net/IServerListener.java
+++ src/net/sf/lipermi/net/IServerListener.java
@@ -1,21 +1,21 @@
 /*
  * LipeRMI - a light weight Internet approach for remote method invocation
  * Copyright (C) 2006  Felipe Santos Andrade
- * 
+ *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
- * 
+ *
  * For more information, see http://lipermi.sourceforge.net/license.php
  * You can also contact author through lipeandrade@users.sourceforge.net
  */
@@ -28,16 +28,18 @@ import java.net.Socket;
  * This listener can be used to monitor a Server.
  * (ie. know when it receives new connections or
  *  close it)
- * 
- * @date   07/10/2006 
+ *
+ * @date   07/10/2006
  * @author lipe
- * 
+ *
  * @see    net.sf.lipermi.net.Server
  */
 public interface IServerListener {
 
-	void clientConnected(Socket socket);
+    void clientConnected(Socket socket);
 
-	void clientDisconnected(Socket socket);
-	
+    void clientDisconnected(Socket socket);
+
 }
+
+// vim: ts=4:sts=4:sw=4:expandtab
